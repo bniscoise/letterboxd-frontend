ad5a5e868d89c5aba3186f5a2883dcf124963226
bniscoise <benjamin.niscoise@consultants-solutec.fr>
2025-09-26 10:42:43 +0200

    Add client-side pagination to movie search

--- src/app/features/movies/search/search.ts
+++ src/app/features/movies/search/search.ts
@@ -1,4 +1,4 @@
-import { Component, signal, inject } from '@angular/core';
+import { Component, signal, inject, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormControl } from '@angular/forms';
 import { debounceTime, distinctUntilChanged, filter, finalize, switchMap, tap } from 'rxjs';
@@ -13,9 +13,25 @@ import { MovieService, MovieDto } from '../../../core/services/movie.service';
 export class SearchComponent {
   queryControl = new FormControl('', { nonNullable: true });
 
+  private readonly pageSize = 10;
+
   movies = signal<MovieDto[]>([]);
   loading = signal(false);
   searchPerformed = signal(false);
+  currentPage = signal(1);
+
+  pagedMovies = computed(() => {
+    const start = (this.currentPage() - 1) * this.pageSize;
+    return this.movies().slice(start, start + this.pageSize);
+  });
+
+  totalPages = computed(() => {
+    return Math.max(1, Math.ceil(this.movies().length / this.pageSize));
+  });
+
+  pageNumbers = computed(() => {
+    return Array.from({ length: this.totalPages() }, (_, index) => index + 1);
+  });
 
   private movieService = inject(MovieService);
 
@@ -35,12 +51,32 @@ export class SearchComponent {
         )
       )
       .subscribe({
-        next: results => this.movies.set(Array.isArray(results) ? results : []),
-        error: () => this.movies.set([]),
+        next: results => {
+          this.currentPage.set(1);
+          this.movies.set(Array.isArray(results) ? results : []);
+        },
+        error: () => {
+          this.currentPage.set(1);
+          this.movies.set([]);
+        }
       });
   }
 
   trackById(index: number, movie: MovieDto) {
     return movie.id;
+  }
+
+  goToPage(page: number) {
+    if (page >= 1 && page <= this.totalPages()) {
+      this.currentPage.set(page);
+    }
+  }
+
+  previousPage() {
+    this.goToPage(this.currentPage() - 1);
+  }
+
+  nextPage() {
+    this.goToPage(this.currentPage() + 1);
   }
 }
