e12606735678b8c61dce0c75fadc1d77d26e5b69
bniscoise <benjamin.niscoise@consultants-solutec.fr>
2025-10-06 09:16:15 +0200

    Test branch (#5)
    * oubli
    
    * liste et notation
    
    * gestion d'erreurs coté front
    
    * oubli
    
    * restructuration du projet
    
    * titres des films en langue originale dès que possible
    
    * bouton information sur les films + extension format html
    
    * page information et bouton supprimer de la liste
    
    * info component

--- src/app/features/movies/search/components/user-movie-list/user-movie-list.component.ts
+++ src/app/features/movies/search/components/user-movie-list/user-movie-list.component.ts
@@ -11,13 +11,20 @@ import { UserMovieDto } from '../../../../../core/services/user-movie.service';
 })
 export class UserMovieListComponent {
   @Input({ required: true }) userMovies: UserMovieDto[] = [];
+  @Input({ required: true }) pagedUserMovies: UserMovieDto[] = [];
   @Input() loading = false;
   @Input() reviewOpen: Record<number, boolean> = {};
   @Input() canToggleReview = false;
+  @Input() currentPage = 1;
+  @Input() totalPages = 1;
+  @Input() pageNumbers: number[] = [];
 
   @Output() readonly reviewToggle = new EventEmitter<number>();
   @Output() readonly viewInformation = new EventEmitter<UserMovieDto>();
   @Output() readonly deleteUserMovie = new EventEmitter<UserMovieDto>();
+  @Output() readonly previousPage = new EventEmitter<void>();
+  @Output() readonly nextPage = new EventEmitter<void>();
+  @Output() readonly goToPage = new EventEmitter<number>();
 
   onToggleReview(movieId: number) {
     this.reviewToggle.emit(movieId);
@@ -28,5 +35,17 @@ export class UserMovieListComponent {
 
   onDeleteMovie(movie: UserMovieDto) {
     this.deleteUserMovie.emit(movie);
+  }
+
+  onPreviousPage() {
+    this.previousPage.emit();
+  }
+
+  onNextPage() {
+    this.nextPage.emit();
+  }
+
+  onGoToPage(page: number) {
+    this.goToPage.emit(page);
   }
 }
