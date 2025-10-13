fc63a4124a3ffd5f44a25660cf0fe6d036673cf3
bniscoise <benjamin.niscoise@consultants-solutec.fr>
2025-10-13 16:54:10 +0200

    accès à la liste perso des autres utilisateurs en cliquant sur leur username

--- src/app/features/movies/movie-information/components/movie-review-list/movie-review-list.component.ts
+++ src/app/features/movies/movie-information/components/movie-review-list/movie-review-list.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { RatingStarsComponent } from '../../../../../shared/ui/rating-stars/rating-stars.component';
 import { UserMovieDto } from '../../../../../core/services/user-movie.service';
 
 @Component({
   selector: 'app-movie-review-list',
   standalone: true,
-  imports: [CommonModule, RatingStarsComponent],
+  imports: [CommonModule, RouterLink, RatingStarsComponent],
   templateUrl: './movie-review-list.component.html',
   styleUrls: ['./movie-review-list.component.css']
 })
